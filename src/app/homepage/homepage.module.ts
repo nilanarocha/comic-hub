d117bb937a04b1da7210a03fb5287c10793a9a88
Nilana Rocha <nilanaarquitetura@gmail.com>
2019-10-31 22:55:55 +1100

    adding cards in homepage

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
-
-
-
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [HomepageComponent],
   imports: [
     CommonModule,
     SharedModule,
+    MatCardModule,
+    MatGridListModule,
     RouterModule.forChild([
       {
         path: '',
