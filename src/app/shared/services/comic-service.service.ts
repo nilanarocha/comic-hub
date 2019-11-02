a74d61b58550449257fb64c2ab6d901e1cc49a75
Nilana Rocha <nilanaarquitetura@gmail.com>
2019-11-02 11:19:38 +1100

    adding api request in comic-service

--- src/app/shared/services/comic-service.service.ts
+++ src/app/shared/services/comic-service.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { MOCK } from './mock';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+
+const API_URL = 'https://propertymecomics.s3.amazonaws.com/comics';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ComicServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getComics(): Observable<any> {
-    return of(MOCK);
+    // In case of the API is down, please uncomment the line below.
+    // return of(MOCK);
+    return this.http.get(API_URL);
   }
+
 }
+
 
 
