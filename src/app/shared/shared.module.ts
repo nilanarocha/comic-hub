3088e53e0788cda8c2c275f9db52a1099bb4073b
Nilana Rocha <nilanaarquitetura@gmail.com>
2019-11-02 17:14:38 +1100

    styling comics-details

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule,
+    MatToolbarModule
   ]
 })
 export class SharedModule { }
