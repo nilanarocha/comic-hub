e9922193185d71f8978a4c4126697531a8bc1ad9
Nilana Rocha <nilanaarquitetura@gmail.com>
2019-11-04 09:23:04 +1100

    adding link in deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,7 @@ echo "Starting deployment"
 echo "..."
 ng deploy --base-href=/comic-hub/
 echo "..."
+echo ""
+echo "New version published. Please check the changes in https://nilanarocha.github.io/comic-hub/"
+echo ""
 echo "All done. Enjoy!"
